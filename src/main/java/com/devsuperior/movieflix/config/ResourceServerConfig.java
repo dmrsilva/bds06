137c54b3307f6423486e50182af45a7c5d6daa8a
Daniel Silva <daniel.mascr@gmail.com>
2022-03-05 22:40:18 -0300

    Paginated listing of movies and filtering by genre

--- src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -23,7 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
-	private static final String[] VISITOR_OR_MEMBER = {"/genres" };
+	private static final String[] VISITOR_OR_MEMBER = {"/genres", "/movies"};
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
